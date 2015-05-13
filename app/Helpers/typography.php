9ff527301702276fa450edd67cd1b580f363a31d
Muhammad Resna Rizki Pratama <resnarizki29@gmail.com>
2015-05-14 03:05:17 +0900

    Add to_rp helper

--- app/Helpers/typography.php
+++ app/Helpers/typography.php
@@ -10,3 +10,8 @@ function parsedown($text, $striptags = false)
     $parsedown = new Parsedown();
     return ($striptags) ? strip_tags($parsedown->parse($text)) : $parsedown->parse($text);
 }
+
+function to_rp($number)
+{
+    return 'Rp. ' . number_format($number, 0, ',', '.');
+}
