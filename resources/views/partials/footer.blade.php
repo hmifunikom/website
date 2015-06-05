3b7d76cb77d9b90a95bba8b0636714bc25007646
Muhammad Resna Rizki Pratama <resnarizki29@gmail.com>
2015-06-06 01:49:29 +0900

    Working on footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,19 +1,30 @@
 <div id="footer" class="footer">
     <div class="container">
-        <div class="footer-brand">
-            <div class="footer-brand-logo"></div>
-            Color Admin
+        <div class="row">
+            <div class="col-md-7">
+                <div class="footer-brand">
+                    <img src="{{ asset_version('assets/images/logo-footer.png') }}" class="m-b-10">
+                </div>
+                <p>
+                    &copy; 2015 Himpunan Teknik Informatika Universitas Komputer Indonesia <br/>
+                    Developed by HMIF Developer | All Right Reserved
+                </p>
+
+                <p class="social-list">
+                    <a href="https://www.facebook.com/hmifunikom"><i class="fa fa-facebook fa-fw"></i></a>
+                    <a href="https://instagram.com/hmifunikom"><i class="fa fa-instagram fa-fw"></i></a>
+                    <a href="https://twitter.com/hmifunikom"><i class="fa fa-twitter fa-fw"></i></a>
+                    <a href="https://plus.google.com/111681065704894169638" rel="publisher"><i
+                                class="fa fa-google-plus fa-fw"></i></a>
+                </p>
+            </div>
+            <div class="col-md-5 text-right">
+                <h3>Hosted by</h3>
+                <a href="http://hostingbdg.com">
+                    <img src="{{ asset_version('assets/images/logo-hosting.png') }}">
+                </a>
+
+            </div>
         </div>
-        <p>
-            &copy; Copyright Color Admin 2014 <br />
-            An admin & front end theme with serious impact. Created by <a href="#">SeanTheme</a>
-        </p>
-        <p class="social-list">
-            <a href="#"><i class="fa fa-facebook fa-fw"></i></a>
-            <a href="#"><i class="fa fa-instagram fa-fw"></i></a>
-            <a href="#"><i class="fa fa-twitter fa-fw"></i></a>
-            <a href="#"><i class="fa fa-google-plus fa-fw"></i></a>
-            <a href="#"><i class="fa fa-dribbble fa-fw"></i></a>
-        </p>
     </div>
 </div>
